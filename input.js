08834841afab482bafb5e82c5377ee78470459ad
Matthew Thompson <supra__charged@hotmail.com>
2021-05-05 20:55:43 +0000

    Add move snake functionality

--- input.js
+++ input.js
@@ -1,10 +1,30 @@
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = function() {
+const handleUserInput = function(key) {
+  switch (key) {
+  case '\u0003':
+    process.exit();
+    break;
+  case 'w':
+    connection.write('Move: up');
+    break;
+  case 'a':
+    connection.write('Move: left');
+    break;
+  case 's':
+    connection.write('Move: down');
+    break;
+  case 'd':
+    connection.write('Move: right');
+    break;
+  }
+  // if (key === '\u0003') {
+  //   process.exit();
+  // }
+}
+
+const setupInput = conn => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
